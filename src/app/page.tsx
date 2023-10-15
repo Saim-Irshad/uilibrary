f3e0149ef5699acce00fa67d2f7569df08e235a6
Muhammad Waleed <waleed.taher17@gmail.com>
2023-10-15 16:10:31 +0500

    feat(tailwind-merge/applied): tailwind merge and clsx applied

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,33 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
   return (
-    <h1 className="h1-bold">Hello saim how are u</h1>
+    <main>
+      <div
+        className={cn(
+          "bg-red-500 text-center flex justify-center gap-2 text-white font-bold",
+          { "text-black": count >= 5 && count < 7 },
+          [count === 3 && "text-green-500"]
+        )}
+      >
+        {count}
+        <button
+          className={
+            count === 3
+              ? "text-green-500"
+              : count === 5
+              ? "text-black"
+              : "text-yellow-400"
+          }
+          onClick={() => setCount(count + 1)}
+        >
+          add 1
+        </button>
+      </div>
+    </main>
   );
 }
